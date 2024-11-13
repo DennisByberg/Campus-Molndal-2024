42e7deb67eaa836ca5e474ecd36ee4733d98d3cd
94denbyb <94denbyb@gafe.molndal.se>
2024-11-13 18:43:39 +0100

    Add project files.

---
+++ campus_molndal_2024_oop/02_basiccsharp/VariablesAndDataTypes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace campus_molndal_2024_oop._02_basiccsharp
+{
+    public static class VariablesAndDataTypes
+    {
+        public static void GetExercise1()
+        {
+            // En heltalsvariabel age med värdet 30.
+            const int age = 30;
+            // En flyttalsvariabel height med värdet 1.75.
+            const float height = 1.75f;
+            // En char-variabel initial med värdet 'A'.
+            const char initial = 'A';
+            // En strängvariabel name med värdet "Alice".
+            const string name = "Alice";
+            // En bool-variabel isStudentmed värdettrue`.
+            const bool isStudent = true;
+
+            Console.WriteLine(age);
+            Console.WriteLine(height);
+            Console.WriteLine(initial);
+            Console.WriteLine(name);
+            Console.WriteLine(isStudent);
+        }
+    }
+}
---
+++ campus_molndal_2024_oop/Program.cs
@@ -0,0 +1,15 @@
+﻿using campus_molndal_2024_oop._02_basiccsharp;
+using System;
+
+namespace campus_molndal_2024_oop
+{
+    static class Program
+    {
+        static void Main()
+        {
+            VariablesAndDataTypes.GetExercise1();
+
+            Console.ReadLine();
+        }
+    }
+}
