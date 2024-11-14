a7e237dbf9448371f3b32f1e6da769c9e4ac9586
94denbyb <94denbyb@gafe.molndal.se>
2024-11-14 23:29:31 +0100

    Refactor.

--- campus_molndal_2024_oop/03_repetition_classes_objects/Classes/Order.cs
+++ campus_molndal_2024_oop/03_repetition_classes_objects/Classes/Order.cs
@@ -2,10 +2,6 @@
 
 namespace campus_molndal_2024_oop._03_repetition_classes_objects
 {
-    // Skapa en ny klass kallad Order med fälten OrderId, Product, Quantity, och TotalPrice.
-    // Skapa en konstruktor som tar ett Product-objekt och en Quantity, och beräknar TotalPrice baserat på produktens pris och kvantitet.
-    // Lägg till logik i konstruktören för att förhindra att en order skapas om Quantity är mindre än 1.
-    // Testa klassen genom att skapa flera Order-objekt och verifiera att all logik fungerar korrekt.
     public class Order
     {
         public int OrderId { get; set; }
