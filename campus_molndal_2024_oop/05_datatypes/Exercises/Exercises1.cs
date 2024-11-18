1db373f2b632449705f57deff09dc1c049a03902
94denbyb <94denbyb@gafe.molndal.se>
2024-11-18 17:35:57 +0100

    Övning 2: Hitta Det Största och Minsta Värdet i en Array (20 min): solved

--- campus_molndal_2024_oop/05_datatypes/Exercises/Exercises1.cs
+++ campus_molndal_2024_oop/05_datatypes/Exercises/Exercises1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Net.WebRequestMethods;
 
 namespace campus_molndal_2024_oop._05_datatypes
 {
@@ -16,6 +17,26 @@ namespace campus_molndal_2024_oop._05_datatypes
                 sum += number;
 
             Console.WriteLine($"Average: {sum / arr.Length}");
+        }
+
+        public static void PrintExercise2()
+        {
+            // Skapa en array med heltal.
+            int[] numbers = { 10, 20, 30, 40, 50, 32, 1, 3 };
+            int[] numbers2 = { 193, 32, 2, 15, 92, 44, 22, 16 };
+            // Skriv en metod för att hitta och returnera det största värdet i arrayen.
+            var biggestNumber = NumberHelper.GetBiggestNumberInArray(numbers);
+            var biggestNumber2 = NumberHelper.GetBiggestNumberInArray(numbers2);
+            // Skriv en metod för att hitta och returnera det minsta värdet i arrayen.
+            var smallestNumber = NumberHelper.GetSmallestNumberInArray(numbers);
+            var smallestNumber2 = NumberHelper.GetSmallestNumberInArray(numbers2);
+            // Testa metoderna med några olika värden.
+            Console.WriteLine("--- numbers ---");
+            Console.WriteLine($"biggestNumber: {biggestNumber}");
+            Console.WriteLine($"smallestNumber: {smallestNumber}");
+            Console.WriteLine("\n--- numbers 2 ---");
+            Console.WriteLine($"biggestNumber: {biggestNumber2}");
+            Console.WriteLine($"smallestNumber: {smallestNumber2}");
         }
     }
 }
---
+++ campus_molndal_2024_oop/05_datatypes/Helpers/NumberHelper.cs
@@ -0,0 +1,29 @@
+﻿namespace campus_molndal_2024_oop._05_datatypes
+{
+    public static class NumberHelper
+    {
+        public static int GetBiggestNumberInArray(int[] arr)
+        {
+            var biggestNumber = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > biggestNumber)
+                    biggestNumber = arr[i];
+            }
+
+            return biggestNumber;
+        }
+
+        public static int GetSmallestNumberInArray(int[] arr)
+        {
+            var smallestNumber = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] < smallestNumber)
+                    smallestNumber = arr[i];
+            }
+
+            return smallestNumber;
+        }
+    }
+}
