ffd1e16807dcf6dc67df9472d80db76e19b021b4
94denbyb <94denbyb@gafe.molndal.se>
2024-11-19 11:23:49 +0100

    Refactor

--- campus_molndal_2024_oop/07_inheritance/Exercises/Exercises2.cs
+++ campus_molndal_2024_oop/07_inheritance/Exercises/Exercises2.cs
@@ -4,6 +4,9 @@ namespace campus_molndal_2024_oop._07_inheritance
 {
     public static class Exercises2
     {
+        // 1. Skapa en klass Calculator med överlagrade metoder Multiply().
+        // 2. En metod ska multiplicera två heltal och en annan ska multiplicera två decimaltal.
+        // 3. Implementera metoderna och testa dem genom att skapa ett objekt av Calculator-klassen och anropa båda versionerna av Multiply().
         public static void PrintExercise1()
         {
             Console.WriteLine(Calculator.Multiply(3, 3));
