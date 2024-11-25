60af113aed1e40c9ecc2d22641cc93b5450ab29e
94denbyb <94denbyb@gafe.molndal.se>
2024-11-26 00:35:26 +0100

    lectures/3_oop_adv/lectures/01_oop_sqlite /3_.md: 1,2 & 3 solved

---
+++ campus_molndal_oop_adv_library/Library.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace campus_molndal_oop_adv_library
+{
+    enum TablesSchema
+    {
+        Books,
+        Borrowers
+    }
+
+    enum BorrowersSchema
+    {
+        Id,
+        Name,
+        Email
+    }
+
+    enum BooksSchema
+    {
+        Id,
+        Title,
+        Author,
+        Publication_Year
+    }
+
+    internal class Library
+    {
+
+        private const string CONNECTION_STRING = "Data Source=library.db;";
+
+        public Library()
+        {
+            try
+            {
+                CreateDatabaseAndTables();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ett fel uppstod vid skapandet av databasen eller tabellen: {ex.Message}");
+            }
+        }
+
+        private void CreateDatabaseAndTables()
+        {
+            using (var connection = new SQLiteConnection(CONNECTION_STRING))
+            {
+                connection.Open();
+
+                string CREATE_BOOKS_TABLE_QUERY = $@"CREATE TABLE IF NOT EXISTS {TablesSchema.Books} (
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Title TEXT NOT NULL,
+                    Author TEXT NOT NULL,
+                    Publication_Year INTEGER NOT NULL
+                );";
+
+                string CREATE_BORROWERS_TABLE_QUERY = $@"
+                    CREATE TABLE IF NOT EXISTS {TablesSchema.Borrowers} (
+                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        Name TEXT NOT NULL,
+                        Email TEXT NOT NULL
+                    );";
+
+                using (var transaction = connection.BeginTransaction())
+                {
+                    using (var command = new SQLiteCommand(CREATE_BOOKS_TABLE_QUERY, connection))
+                    {
+                        command.ExecuteNonQuery();
+                    }
+
+                    using (var command = new SQLiteCommand(CREATE_BORROWERS_TABLE_QUERY, connection))
+                    {
+                        command.ExecuteNonQuery();
+                    }
+
+                    transaction.Commit();
+                }
+            }
+        }
+
+        public void AddBook(string title, string author, int publicationYear)
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(CONNECTION_STRING))
+                {
+                    connection.Open();
+
+                    string INSERT_INTO_BOOKS_QUERY =
+                       $"INSERT INTO {TablesSchema.Books}(Title, Author, Publication_Year) VALUES (@Title, @Author, @Publication_Year)";
+
+                    using (var command = new SQLiteCommand(INSERT_INTO_BOOKS_QUERY, connection))
+                    {
+                        command.Parameters.AddWithValue("@Title", title);
+                        command.Parameters.AddWithValue("@Author", author);
+                        command.Parameters.AddWithValue("@Publication_Year", publicationYear);
+
+                        command.ExecuteNonQuery();
+
+                        Console.WriteLine("Book Added");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        public void AddBorrower(string name, string email)
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(CONNECTION_STRING))
+                {
+                    connection.Open();
+
+                    string QUERY = $@"
+                    INSERT INTO {TablesSchema.Borrowers}(Name, Email)
+                    VALUES(@Name, @Email)";
+
+                    using (var command = new SQLiteCommand(QUERY, connection))
+                    {
+                        command.Parameters.AddWithValue("@Name", name);
+                        command.Parameters.AddWithValue("@Email", email);
+
+                        command.ExecuteNonQuery();
+
+                        Console.WriteLine("Borrower Added");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        public void DisplayBooks()
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(CONNECTION_STRING))
+                {
+                    connection.Open();
+
+                    string QUERY = $"SELECT * FROM {TablesSchema.Books}";
+
+                    using (var command = new SQLiteCommand(QUERY, connection))
+                    {
+                        var reader = command.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"{reader[$"{BooksSchema.Id}"]} | {reader[$"{BooksSchema.Title}"]} | {reader[$"{BooksSchema.Author}"]} | {reader[$"{BooksSchema.Publication_Year}"]}");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        public void DisplayBorrowers()
+        {
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(CONNECTION_STRING))
+                {
+                    connection.Open();
+
+                    string query = $"SELECT * FROM {TablesSchema.Borrowers}";
+
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                    {
+                        SQLiteDataReader reader = command.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"{reader[$"{BorrowersSchema.Id}"]} | {reader[$"{BorrowersSchema.Name}"]} | {reader[$"{BorrowersSchema.Email}"]}");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ campus_molndal_oop_adv_library/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace campus_molndal_oop_adv_library
+{
+    internal static class Program
+    {
+        static void Main()
+        {
+            Library library = new Library();
+
+            while (true)
+            {
+                Console.WriteLine("1. Add Book");
+                Console.WriteLine("2. Add Borrower");
+                Console.WriteLine("3. Display Books");
+                Console.WriteLine("4. Display Borrowers");
+                Console.WriteLine("5. Exit");
+                Console.Write(">> ");
+
+                // Kontrollera om inmatningen är en giltig siffra
+                if (!int.TryParse(Console.ReadLine(), out int choice))
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                    continue;
+                }
+
+                switch (choice)
+                {
+                    // Add Book
+                    case 1:
+                        Console.Write("Enter Title: ");
+                        var title = Console.ReadLine();
+
+                        Console.Write("Enter Author: ");
+                        var author = Console.ReadLine();
+
+                        Console.Write("Enter Publication Year: ");
+                        if (!int.TryParse(Console.ReadLine(), out int publicationYear))
+                        {
+                            Console.WriteLine("Invalid input for Publication Year. Please try again.");
+                            continue;
+                        }
+
+                        library.AddBook(title, author, publicationYear);
+                        break;
+
+                    // Add Borrower
+                    case 2:
+                        Console.Write("Enter Name: ");
+                        var name = Console.ReadLine();
+
+                        Console.Write("Enter Email: ");
+                        var email = Console.ReadLine();
+
+                        library.AddBorrower(name, email);
+                        break;
+
+                    // Display Books
+                    case 3:
+                        Console.WriteLine("\n-");
+                        library.DisplayBooks();
+                        Console.WriteLine("-\n");
+                        break;
+
+                    // Display Borrowerss
+                    case 4:
+                        Console.WriteLine("\n-");
+                        library.DisplayBorrowers();
+                        Console.WriteLine("-\n");
+                        break;
+
+                    // Exit.
+                    case 5:
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        continue;
+                }
+            }
+        }
+    }
+}
